df38296d6d28f2f877c036c16853de478aa6ee18
r1oga <r1oga@tuta.io>
2020-09-06 19:34:57 +0200

    feat: Update and delete items

--- script.js
+++ script.js
@@ -43,11 +43,19 @@ function storeKanban() {
 }
 
 // Create DOM Elements for each list item
-function createItemEl(columnEl, item) {
+function createItemEl(columnEl, text) {
   const listEl = document.createElement('li')
   listEl.classList.add('drag-item')
-  listEl.textContent = item
+  listEl.textContent = text
   listEl.draggable = true
+  listEl.contentEditable = true
+  listEl.addEventListener('focusout', event => {
+    if (event.target.textContent === '') {
+      listEl.remove()
+    }
+    updateLocalKanbanObject()
+    storeKanban()
+  })
   listEl.setAttribute('ondragstart', 'drag(event)')
   columnEl.appendChild(listEl)
 }
@@ -88,6 +96,30 @@ const updateLocalKanbanObject = () => {
       ...document.getElementById(`${column}-list`).children
     ].map(item => item.textContent)
   })
+}
+
+const addToColumn = name => {
+  const index = Object.keys(kanban).indexOf(name)
+  const text = addItems[index].textContent
+  createItemEl(document.getElementById(`${name}-list`), text)
+  updateLocalKanbanObject()
+  storeKanban()
+  addItems[index].textContent = ''
+}
+
+const showInputBox = name => {
+  const index = Object.keys(kanban).indexOf(name)
+  addBtns[index].style.visibility = 'hidden'
+  saveItemBtns[index].style.display = 'flex'
+  addItemContainers[index].style.display = 'flex'
+}
+
+const hideInputBox = name => {
+  const index = Object.keys(kanban).indexOf(name)
+  addBtns[index].style.visibility = 'visible'
+  saveItemBtns[index].style.display = 'none'
+  addItemContainers[index].style.display = 'none'
+  addToColumn(name)
 }
 
 // Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
